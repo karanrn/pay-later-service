fe5f4dc18d50510d8d1eb7c43e96c1d7e844301b
Karan Nadagoudar <karan4080@gmail.com>
2020-09-24 10:07:54 +0530

    Used Flagset

--- new/main.go
+++ new/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -18,72 +19,82 @@ func main() {
 	var newUser user.User
 	var newMerchant merchant.Merchant
 
-	newUsage := `For adding a new user/merchant: 
-	new user user_id email_id credit-limit
-	ex: new user user1 u1@users.com 500
-	For adding a new merchant: 
-	new merchant merchant_id discount-percentage
-	ex: new merchant m1 email_id 10%
-	For new transaction:
-	new txn user_id merchant_id txn-amount0
-	ex: new txn user1 m1 150`
-	/*
-		updateUsage := `For updating merchant information - discount-percentage:
-		update merchant merchant_id new_discount_percentage
-		ex: update merchant m1 5%`
-		reportUsage := `For getting report of the system:
-		report [discount dues total-dues users-at-credit-limit] [user_id merchant_id]
-		ex: report discount m1
-			report dues user1`
-	*/
 	// New subcommands or flags
-	userID := flag.String("user", "", newUsage)
-	email := flag.String("email", "", newUsage)
-	creditLimit := flag.Int64("credit-limit", 0, newUsage)
+	userCmd := flag.NewFlagSet("user", flag.ContinueOnError)
+	userID := userCmd.String("id", "", "User ID: alphanumeric value. ex: -user user101")
+	email := userCmd.String("email", "", "Email ID of user: xyz@domain.com. ex: -email abc@xyz.com")
+	creditLimit := userCmd.Int64("credit-limit", 0, "Credit limit for the user, default is 0. ex: -credit-limit 1000")
 
-	merchantID := flag.String("merchant", "", newUsage)
-	discount := flag.String("discount", "0%", newUsage)
-	flag.Parse()
+	merchantCmd := flag.NewFlagSet("merchant", flag.ContinueOnError)
+	merchantID := merchantCmd.String("id", "", "Merchant ID: alphanumeric value. ex: -merchant m101")
+	mEmail := merchantCmd.String("email", "", "Email ID of merchant: xyz@domain.com. ex: -email abc@xyz.com")
+	discount := merchantCmd.String("discount", "0%", "Discount offered by merchant. ex: -discount 5%")
 
-	if *userID != "" {
-		emailCheck := isEmailValid(*email)
-		if !emailCheck {
-			fmt.Printf("%s - Not a valid email address.", *email)
-			return
-		}
+	txn := flag.NewFlagSet("txn", flag.ContinueOnError)
+	tUser := txn.String("user-id", "", "User involved in transaction. ex: -user-id user101")
+	tMerchant := txn.String("merchant-id", "", "Merchant involved in transaction. ex: -merchant-id m101")
+	tAmt := txn.Int64("amt", 0, "Transaction amount. ex: -amt 100")
 
-		newUser.UserID = *userID
-		newUser.EmailID = *email
-		newUser.CreditLimit = *creditLimit
-
-		err := user.Add(newUser)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("You must pass sub command - [user, merchant, txn]")
+		return
 	}
 
-	if *merchantID != "" {
-		newMerchant.MerchantID = *merchantID
+	switch os.Args[1] {
+	case "user":
+		if err := userCmd.Parse(os.Args[2:]); err == nil {
+			if *userID != "" {
+				emailCheck := isEmailValid(*email)
+				if !emailCheck {
+					fmt.Printf("%s - Not a valid email address.", *email)
+					return
+				}
+				if *creditLimit == 0 {
+					fmt.Printf("Credit Limit cannot be zero (Default: zero).")
+					return
+				}
+				newUser.UserID = *userID
+				newUser.EmailID = *email
+				newUser.CreditLimit = *creditLimit
 
-		emailCheck := isEmailValid(*email)
-		if !emailCheck {
-			fmt.Printf("%s - Not a valid email address.", *email)
-			return
+				err := user.Add(newUser)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
 		}
-		newMerchant.EmailID = *email
+	case "merchant":
+		if err := merchantCmd.Parse(os.Args[2:]); err == nil {
+			if *merchantID != "" {
+				newMerchant.MerchantID = *merchantID
 
-		disAmt, err := strconv.ParseInt(strings.TrimRight(*discount, "%"), 10, 64)
-		if err != nil {
-			fmt.Printf("%s - Invalid discount type. Should of format 10%%", *discount)
+				emailCheck := isEmailValid(*mEmail)
+				if !emailCheck {
+					fmt.Printf("%s - Not a valid email address.", *email)
+					return
+				}
+				newMerchant.EmailID = *mEmail
+
+				disAmt, err := strconv.ParseInt(strings.TrimRight(*discount, "%"), 10, 64)
+				if err != nil {
+					fmt.Printf("%s - Invalid discount type. Should of format 10%%", *discount)
+				}
+				newMerchant.Discount = disAmt
+
+				err = merchant.Add(newMerchant)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
 		}
-		newMerchant.Discount = disAmt
-
-		err = merchant.Add(newMerchant)
-		if err != nil {
-			fmt.Println(err.Error())
+	case "txn":
+		if err := txn.Parse(os.Args[2:]); err == nil {
+			if *tUser != "" && *tMerchant != "" {
+				// TO DO - implement transaction logic
+				fmt.Printf("Transaction amount: %d", *tAmt)
+			}
 		}
 	}
-
 }
 
 func isEmailValid(email string) bool {
